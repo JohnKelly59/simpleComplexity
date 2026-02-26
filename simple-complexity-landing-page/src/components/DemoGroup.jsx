d268f9089a1270593081183a2a487f74bf258378
John Kelly <john.kellyfitness@gmail.com>
2026-02-25 22:12:39 -0500

    update images

--- simple-complexity-landing-page/src/components/DemoGroup.jsx
+++ simple-complexity-landing-page/src/components/DemoGroup.jsx
@@ -6,19 +6,22 @@ import 'swiper/css/pagination';
 import 'swiper/css/effect-coverflow';
 
 // Import screenshots
-import screenshot1 from '../assets/screenshots/IMG_5321.png';
-import screenshot2 from '../assets/screenshots/IMG_5323.png';
-import screenshot3 from '../assets/screenshots/IMG_5325.png';
-import screenshot4 from '../assets/screenshots/IMG_5327.png';
-import screenshot5 from '../assets/screenshots/IMG_5352.png';
+import screenshot1 from '../assets/screenshots/screenshot1.png';
+import screenshot2 from '../assets/screenshots/screenshot2.png';
+import screenshot3 from '../assets/screenshots/screenshot3.png';
+import screenshot4 from '../assets/screenshots/screenshot4.png';
+import screenshot5 from '../assets/screenshots/screenshot5.png';
+import screenshot6 from '../assets/screenshots/screenshot6.png';
+import screenshot7 from '../assets/screenshots/screenshot7.png';
 
 const screenshots = [
-     { src: screenshot5, alt: 'App Screenshot 5' },
-         { src: screenshot3, alt: 'App Screenshot 3' },
     { src: screenshot1, alt: 'App Screenshot 1' },
-    { src: screenshot2, alt: 'App Screenshot 2' },  
+    { src: screenshot2, alt: 'App Screenshot 2' },
+    { src: screenshot3, alt: 'App Screenshot 3' },
     { src: screenshot4, alt: 'App Screenshot 4' },
-  
+    { src: screenshot5, alt: 'App Screenshot 5' },
+    { src: screenshot6, alt: 'App Screenshot 6' },
+    { src: screenshot7, alt: 'App Screenshot 7' },
 ];
 
 const DemoGroup = () =>
